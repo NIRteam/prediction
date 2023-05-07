7aeb2d6aded3610c70bf8a5eb838d037b96d65e6
Savelov Dmitry <savasavelck@mail.ru>
2023-05-07 13:28:55 +0300

    deleted unnecessary files in DMVFN

--- DMVFN/model.py
+++ DMVFN/model.py
@@ -12,8 +12,8 @@ root_path = os.path.abspath(__file__)
 root_path = '/'.join(root_path.split('/')[:-2])
 sys.path.append(root_path)
 
-from model.arch import *
-from loss.loss import *
+from .arch import *
+from .loss import *
 
 device = torch.device("cuda")
     
