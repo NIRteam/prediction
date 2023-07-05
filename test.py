e2cb410046b25ad6ec4c56e66cecf2900577610e
vanotuvanov <62586763+vanotuvanov@users.noreply.github.com>
2023-07-05 18:41:22 +0300

    minor fixes

--- test.py
+++
@@ -1,5 +0,0 @@
-from model.models import Model, DMVFN
-
-if __name__ == '__main__':
-    model = Model(DMVFN(load_path = "model/pretrained_models/dmvfn_city.pkl", device="cpu"))
-    model.predictVideo(video_path='C:/Users/tuman/Desktop/test_clip/test_2.mp4', real=1, fake=1, save_path=None)
